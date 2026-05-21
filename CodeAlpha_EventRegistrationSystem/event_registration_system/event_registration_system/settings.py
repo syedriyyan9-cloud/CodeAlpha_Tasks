79d35bad737329f4c2d1d6c0b4865f7f3546bb2b
Syed Riyyan Hassan <riyyanhassan789@gmail.com>
2026-05-21 18:02:50 +0500

    Migrations were made

--- CodeAlpha_EventRegistrationSystem/event_registration_system/event_registration_system/settings.py
+++ CodeAlpha_EventRegistrationSystem/event_registration_system/event_registration_system/settings.py
@@ -12,12 +12,14 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 import dj_database_url
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env_path = Path(__file__).resolve().parent.parent / '.env'
+load_dotenv(env_path)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -96,6 +98,8 @@ DATABASES = {
         conn_health_checks=True,
     )
 }
+
+# TEMPORARY - Just for running migrations locally
 
 
 # Password validation
